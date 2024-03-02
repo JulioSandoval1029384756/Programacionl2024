4be606933b5a481f696a55dcbe3a87d3010721d6
JulioSandoval1029384756 <135393015+JulioSandoval1029384756@users.noreply.github.com>
2024-03-02 10:43:30 -0600

    Add files via upload

---
+++ PrimerParcial/Serie 2/Serie 2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Serie_2
+{
+    public class Operaciones
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la cantidad de numeros que desea: ");
+            int CantidadDeNumeros = int.Parse(Console.ReadLine());
+
+            int[] numeros = new int[CantidadDeNumeros];
+            int NumerosPares = 0;
+            int NumerosImpares = 0;
+            int SumaPares = 0;
+            int SumaImpares = 0;
+
+            for (int i = 0; i < CantidadDeNumeros; i++)
+            {
+                Console.Write($"Ingrese el numero {i + 1}: ");
+                numeros[i] = int.Parse(Console.ReadLine());
+
+                if (numeros[i] % 2 == 0)
+                {
+                    NumerosPares++;
+                    SumaPares += numeros[i];
+                }
+                else
+                {
+                    NumerosImpares++;
+                    SumaImpares += numeros[i];
+                }
+            }
+
+            Console.WriteLine("\nCantidad numeros pares: " + NumerosPares + " \nSuma numeros pares: " + SumaPares + " \nCantidad numeros impares: " + NumerosImpares + " \nSuma impares: " + SumaImpares);
+        }
+    }
+}
