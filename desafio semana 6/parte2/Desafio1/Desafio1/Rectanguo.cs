af00f86d374a4cd0ecabf85147f2ccecd80d59d6
JulioSandoval1029384756 <135393015+JulioSandoval1029384756@users.noreply.github.com>
2024-03-15 15:40:38 -0600

    Add files via upload

---
+++ desafio semana 6/parte2/Desafio1/Desafio1/Rectanguo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio1
+{
+    using System;
+
+    class Rectangulo
+    {
+        
+        public double Alto { get; }
+        public double Largo { get; }
+
+        
+        public double SuperficieFrontal => Alto * Largo;
+
+        
+        public Rectangulo(double alto, double largo)
+        {
+            Alto = alto;
+            Largo = largo;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Rectangulo rectangulo = new Rectangulo(30, 60);
+
+            
+            Console.WriteLine($"Superficie frontal del rectángulo: {rectangulo.SuperficieFrontal}");
+        }
+    }
+
+}
